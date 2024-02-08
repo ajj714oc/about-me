06a9002b3a289e514f03c80c6911c7ad2d6bdbe8
Adam Jimenez <outlandioc@gmail.com>
2024-02-08 12:56:42 -0800

    revising js

--- JavaScript-folder/main.js
+++ JavaScript-folder/main.js
@@ -6,13 +6,13 @@
 
  
 
-    function didYouEnjoyYourVisit() {
-        let visit = confirm("Did you enjoy your visit to my site today?");
-        if (visit === "yes" || visit === "No") {
-            document.write("Would you consider visiting again?");
-        } else {
-            document.write("Thank you for your honest opinion!");
-        }
+ function didYouEnjoyYourVisit() {
+    let visit = confirm("Did you enjoy your visit to my site today?");
+    if (visit) {
+        // User clicked "OK" (true)
+        alert("Great! Would you consider visiting again?");
+    } else {
+        // User clicked "Cancel" (false)
+        alert("Thank you for your honest opinion!");
     }
-
-   
+}
